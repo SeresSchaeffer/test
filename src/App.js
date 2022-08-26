3ed0bac3d5b6e10615d49f16785b98e42f2faafc
SeresSchaeffer <o0peetarza0o@gmail.com>
2022-08-26 10:52:00 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { Content, Footer, Header } from './func-components';
+
+function App() {
+  return (
+    [<Header/>,[<Content/>],[<Footer/>]]
+    
+    
+  );
+}
+
+export default App;
---
+++ src/func-components.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+export function Header(){
+    const aStyle = {
+        display:'inline-block',
+        color:'blue',
+        margin:'7px',
+        TextDecoration:'none'
+    }
+
+    return(
+        <div style={{blackgroundColor:'#ccc'}}>
+            <a href='' style={aStyle}>Home</a>|&nbsp;
+            <a href='' style={aStyle}>React</a>|&nbsp;
+            <a href='' style={aStyle}>React Native</a>
+        </div>
+    )
+}
+
+export function Content(){
+    return(
+        <div style={{textAlign:'center'}}>
+            <h2>Hello is me Mario!!!<br/>React & React Native</h2>
+            <br/><br/><br/>
+        </div>
+    )
+}
+
+export const Footer = () =>{
+    return(
+        <div style={{textAlign:'center'}}>
+            &copy;{new Date().getFullYear()}All right reserved
+        </div>
+    )
+}
+
